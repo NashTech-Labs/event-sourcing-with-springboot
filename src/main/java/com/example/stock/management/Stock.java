46e78a3ce23852e44e4739a94f20b0a78e1daf14
Gaurav Dubey <mail.gaurav397@gmail.com>
2022-09-13 11:18:40 +0530

    event sourcing with springboot

---
+++ src/main/java/com/example/stock/management/Stock.java
@@ -0,0 +1,17 @@
+package com.example.stock.management;
+
+import lombok.Data;
+
+//entity model
+@Data
+public class Stock {
+
+	private String name;
+
+	private int quantity;
+
+	private String user;
+	
+	
+
+}
---
+++ src/main/java/com/example/stock/management/StockmanagementEventstoreApplication.java
@@ -0,0 +1,14 @@
+package com.example.stock.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main class of the application
+@SpringBootApplication
+public class StockmanagementEventstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockmanagementEventstoreApplication.class, args);
+	}
+
+}
